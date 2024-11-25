3d98305dd41c27ba2fb8dae5d8ebf3fe7d83d693
Jupp Szepaniak <sze.jupp2001@gmail.com>
2024-11-25 18:33:39 +0100

    trial and error + small changes
    - added accordion for diary entry

--- script.js
+++ script.js
@@ -4,3 +4,28 @@ textarea.addEventListener("keyup", e =>{
     let scHeight = e.target.scrollHeight;
     textarea.style.height = `${scHeight}px`;
 });
+
+const accordionHeaders = document.querySelectorAll(".notes-container-header");
+const accordionContents = document.querySelectorAll(".notes-container-content");
+
+accordionHeaders.forEach((header) => {
+    header.addEventListener("click", () => {
+        const accordionItem = header.parentElement;
+        const accordionContent = accordionItem.querySelector(".notes-container-content");
+    
+        accordionContents.forEach((content => {
+            if (content !== accordionContent) {
+                content.classList.remove("active");
+                content.style.maxHeight = "0";
+            }
+        }));
+
+        accordionContent.classList.toggle("active");
+
+        if (accordionContent.classList.contains("active")) {
+            accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";  
+        } else {
+            accordionContent.style.maxHeight = "0";
+        }
+    });
+});

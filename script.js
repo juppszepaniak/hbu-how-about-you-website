c1e8f0c7a91644a9b58ff1ed973dd76580108289
Jupp Szepaniak <sze.jupp2001@gmail.com>
2024-12-17 18:21:12 +0100

    main slider functionality
    - main slider functionality is working
    - dashboard no function yet
    - having a problem with loading the diary entries from localstorage

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ function toggleMenu() {
   icon.classList.toggle("open");
 }
 
+/* RegEx */
+
+const removeSpecialChars = (val) => {
+  return val.trim().replace(/[^A-Za-z0-9\-\s]/g, "");
+};
+
 /* SELBSTEINSCHÄTZUNG */
 
 const mentalHealthScale = [
@@ -21,7 +27,11 @@ const mentalHealthScale = [
   "euphorisch",
 ];
 
-const selfAssessmentSlider = document.getElementById("self-assessment-slider");
+const averageMentalHelth = [];
+
+/* SLIDER BY DEFAULT */
+
+const selfAssessmentSlider = document.getElementById("slider-container");
 
 defaultValue =
   selfAssessmentSlider.max < selfAssessmentSlider.min
@@ -29,15 +39,89 @@ defaultValue =
     : selfAssessmentSlider.min +
       (selfAssessmentSlider.max - selfAssessmentSlider.min) / 2;
 
-const input = document.querySelector("#self-assessment");
-const value = document.querySelector("#value");
-const mentaleState = mentalHealthScale[input.value];
+/* SELF ASSESSMENT FUNCTIONS */
 
-console.log(mentaleState);
+const input = document.querySelector("#self-assessment-slider");
+const value = document.querySelector("#slider-value");
 
 value.textContent = mentalHealthScale[input.value];
 input.addEventListener("input", (event) => {
   value.textContent = mentalHealthScale[event.target.value];
 });
 
-function safeSelfAssessment() {}
+/* -------- */
+
+function safeSelfAssessment() {
+  const date = new Date().toLocaleDateString("de-DE"); //= "Value" >> {"date: date"}
+  const emotion = mentalHealthScale[input.value]; //= "Value" >> {"emotion: emotion"}
+  const selfAssessmentId = removeSpecialChars(`${date}`); //= "key" >> selfAssessmentId
+
+  console.log(input.value);
+
+  if (emotion) {
+    const selfAssessment = { date, emotion };
+    localStorage.setItem(
+      `sa-${selfAssessmentId}`,
+      JSON.stringify(selfAssessment)
+    );
+    alert(`Es wurde "${emotion}" für heute, den ${date} gespeichert!`);
+  } else {
+    alert("Da hat irgendwas nicht geklappt!");
+  }
+}
+
+/* -------- */
+
+function updateSelfAssessment() {
+  const sliderValue = document.getElementById("slider-value"); //innerText
+  const allSelfAssessmentsEmotions = [];
+
+  const date = new Date().toLocaleDateString("de-DE");
+  const curSelfAssessmentId = removeSpecialChars(`${date}`);
+
+  for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
+
+    if (key.startsWith("sa-")) {
+      const selfAssessment = JSON.parse(localStorage.getItem(key));
+      const selfAssessmentDate = removeSpecialChars(`${selfAssessment.date}`);
+      const selfAssessmentEmotion = selfAssessment.emotion;
+
+      if (selfAssessmentDate === curSelfAssessmentId) {
+        allSelfAssessmentsEmotions.push(selfAssessmentEmotion);
+      } else {
+        allSelfAssessmentsEmotions.unshift(selfAssessmentEmotion);
+      }
+    }
+  }
+
+  const latestEmotion =
+    allSelfAssessmentsEmotions[allSelfAssessmentsEmotions.length - 1];
+
+  if (document.getElementById("slider-value")) {
+    sliderValue.innerText = latestEmotion;
+    let latestEmotionIndex = 4;
+
+    if (latestEmotion === "hoffnungslos") {
+      latestEmotionIndex = 0;
+    } else if (latestEmotion === "antriebslos") {
+      latestEmotionIndex = 1;
+    } else if (latestEmotion === "traurig") {
+      latestEmotionIndex = 2;
+    } else if (latestEmotion === "unzufrieden") {
+      latestEmotionIndex = 3;
+    } else if (latestEmotion === "neutral") {
+      latestEmotionIndex = 4;
+    } else if (latestEmotion === "zufrieden") {
+      latestEmotionIndex = 5;
+    } else if (latestEmotion === "optimistisch") {
+      latestEmotionIndex = 6;
+    } else if (latestEmotion === "glücklich") {
+      latestEmotionIndex = 7;
+    } else if (latestEmotion === "euphorisch") {
+      latestEmotionIndex = 8;
+    }
+    document.getElementById("self-assessment-slider").value = latestEmotionIndex;
+  }
+}
+window.addEventListener("DOMContentLoaded", updateSelfAssessment);
